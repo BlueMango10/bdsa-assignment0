8cf9095087e3eec4c618a7fa0c9cc56bcc6c640c
Magnus Dalsgaard Larsen <daml@itu.dk>
2021-09-06 14:01:42 +0200

    Add tests

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -4,9 +4,9 @@ namespace Exercise
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
 
         ///<summary>
