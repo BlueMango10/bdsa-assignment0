7a9c331d7f35e834a567f54f19bad4c817727bd2
Magnus Dalsgaard Larsen <daml@itu.dk>
2021-09-06 13:30:10 +0200

    Complete leap year function

--- Exercise.Test/UnitTest.cs
+++ Exercise.Test/UnitTest.cs
@@ -5,10 +5,35 @@ namespace Exercise.Test
 {
     public class UnitTest
     {
+        /*
+        Rules:
+        1: Divisible by 4 => leap year
+        2: Divisible by 100 => not leap year (overrides rule 1)
+        3: Divisible by 400 => leap year (overrides rule 2)
+        */
+
+        [Fact]
+        public void RandomNonLeapYear()
+        {
+            Assert.False(Program.IsLeapYear(6));
+        }
+
         [Fact]
         public void DivisibleByFour()
         {
             Assert.True(Program.IsLeapYear(16));
+        }
+
+        [Fact]
+        public void DivisibleBy100()
+        {
+            Assert.False(Program.IsLeapYear(600));
+        }
+
+        [Fact]
+        public void DivisibleBy400()
+        {
+            Assert.True(Program.IsLeapYear(1600));
         }
     }
 }
--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -12,8 +12,15 @@ namespace Exercise
         ///<summary>
         /// Is 'year' a leap year?
         ///</summary>
-        public static bool IsLeapYear(int year) {
-            throw new NotImplementedException();
+        public static bool IsLeapYear(int year)
+        {
+            if (year % 400 == 0)
+                return true;
+            if (year % 100 == 0)
+                return false;
+            if (year % 4 == 0)
+                return true;
+            return false;
         }
     }
 }
