8b57c5c04b9fa9a3e610d242ece1e14676ddb945
Magnus Dalsgaard Larsen <45166031+BlueMango10@users.noreply.github.com>
2021-09-06 13:32:46 +0200

    Add leap year function (#2)
    * Add leap year function
    
    Co-authored-by: Magnus Dalsgaard Larsen <daml@itu.dk>

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -2,11 +2,25 @@
 
 namespace Exercise
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        ///<summary>
+        /// Is 'year' a leap year?
+        ///</summary>
+        public static bool IsLeapYear(int year)
+        {
+            if (year % 400 == 0)
+                return true;
+            if (year % 100 == 0)
+                return false;
+            if (year % 4 == 0)
+                return true;
+            return false;
         }
     }
 }
